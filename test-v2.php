7d2773045ba46820f818b977b86301d9479b63ee
ber34 <ber34@o2.pl>
2021-02-12 18:17:45 +0100

    Create test-v2.php

---
+++ test-v2.php
@@ -0,0 +1,16 @@
+<?php ?>
+
+<form id="" method="post" action="test-post.php" enctype="multipart/form-data"><label>Wczytaj Plik Obcy:</label><br>
+<input type="text" name="ZBIOR" placeholder="ZBIOR && PODZBIÓR" required="required" /><b style="color:red;">* Zbiór znaczników podzielony &&</b><br>
+<input type="text" name="MARKA" placeholder="MARKA" required="required" /><b style="color:red;">* Tytu&#322;</b><br>
+<input type="text" name="MODEL" placeholder="MODEL" required="required" /><b style="color:red;">*</b><br>
+<input type="text" name="CENA" placeholder="CENA" required="required" /><b style="color:red;">*</b><br>
+<input type="text" name="CENA_NETTO" placeholder="CENA_NETTO" /><br>
+<input type="text" name="VAT" placeholder="VAT" /><br>
+<input type="text" name="ID" placeholder="ID" /><br>
+<input type="text" name="URL_IMG" placeholder="URL_IMG" required="required" /><b style="color:red;">*</b><br>
+<input type="text" name="URL_PROD" placeholder="URL_PROD" /><br>
+<input type="text" name="OPIS" placeholder="OPIS" required="required" /><b style="color:red;">*</b><br>
+<input type="file" name="fileimportxml" accept="text/xml" placeholder="wczytaj plik" required="required" />
+<input type="submit" id="" value="Zapisz >>" class="przycisk" /></form>
+    <br><br><hr>
